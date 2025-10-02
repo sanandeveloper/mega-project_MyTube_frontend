1fc0129d7999e39961de21e088f13c0346edaab8
sanandeveloper <sanankhan201516@gmail.com>
2025-10-02 19:29:55 +0500

    updated

--- src/components/store/subscriber.js
+++ src/components/store/subscriber.js
@@ -2,68 +2,73 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 
-export const userSubscribed=createAsyncThunk("subscriber",async(channelId)=>{
+const API = import.meta.env.VITE_API_BASE_URL;
 
-
+export const userSubscribed = createAsyncThunk(
+  "subscriber/subscribe",
+  async (channelId, { rejectWithValue }) => {
     try {
-        const token=localStorage.getItem('accessToken')
+      const token = localStorage.getItem("accessToken");
 
-        const response= await axios.post(`http://localhost:8000/api/v1/subscriber/${channelId}`,{},
-            {
-                headers:{
-                    Authorization:token
-                }
-             
-            }
-        )
-        console.log("subscribed.data,",response.data)
-        return response.data?.data.totalSubscriber
-    } catch (error) {
-        console.log("somethimng went wrong")
-    }
-
-})
-
-export const userUnsubscribed=createAsyncThunk("unsubscribed",async(channelId)=>{
-
-    try {
-
-        const token =localStorage.getItem("accessToken")
-        const response= await axios.delete(`http://localhost:8000/api/v1/subscriber/delete/${channelId}`,{
-
-            headers:{
-                Authorization:token
-            }
-        })
-
-        return true 
-    } catch (error) {
-        console.log("something went wrong",error)
-    }
-
-}) 
-
-
-const initialState={
-    subscriber:null,
-    error:null
-}
-
-
-const subscriberData=createSlice(
-    {
-        name:"subs",
-        initialState,
-        extraReducers:(builder)=>{
-          builder
-          .addCase(userSubscribed.fulfilled,(state,action)=>{
-
-            state.subscriber=action.payload
-            console.log("subscriber total",state.subscriber)
-          })
-
+      const response = await axios.post(
+        `${API}${import.meta.env.VITE_SUBSCRIBE_ENDPOINT}/${channelId}`,
+        {},
+        {
+          headers: {
+            Authorization: token,
+          },
         }
-    }
-)
+      );
 
-export default subscriberData
+      console.log("subscribed.data:", response.data);
+      return response.data?.data.totalSubscriber;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
+
+export const userUnsubscribed = createAsyncThunk(
+  "subscriber/unsubscribe",
+  async (channelId, { rejectWithValue }) => {
+    try {
+      const token = localStorage.getItem("accessToken");
+
+      await axios.delete(
+        `${API}${import.meta.env.VITE_UNSUBSCRIBE_ENDPOINT}/${channelId}`,
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+
+      return true;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
+
+const initialState = {
+  subscriber: null,
+  error: null,
+};
+
+const subscriberData = createSlice({
+  name: "subs",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(userSubscribed.fulfilled, (state, action) => {
+        state.subscriber = action.payload;
+        console.log("subscriber total", state.subscriber);
+      })
+      .addCase(userUnsubscribed.fulfilled, (state) => {
+        state.subscriber = null;
+      });
+  },
+});
+
+export default subscriberData.reducer;
